d11b16707e7a3a37cad732de5b40202fb0d0f1cb
“[Malavika <[n.malavika@yahoo.com]>
2020-12-02 09:34:32 -0800

    Removed special chars from artists and rows with empty song names

--- pre_processing_music.R
+++ pre_processing_music.R
@@ -14,18 +14,39 @@ ggplot(data=sample_music)+ geom_point(aes(x=loudness, y=popularity))
 
 #removing nulls
 music.df[is.na(music.df)] <- 0
+music.df <- music.df[!(music.df$name==""), ]
 
 #filtering out rows with incomplete date
-temp.df <- grep("^[0-9]*?-[0-9]*?-[0-9]*?", music$release_date, value=TRUE, ignore.case=T, perl=T) #extracting all complete
-date_index <- which(music$release_date %in% temp.df) #finding indexed of the dates extracted in the last line
-processed.df <- music[date_index, ] #filtering the dataset using the indexes obtained
+temp.df <- grep("^[0-9]*?-[0-9]*?-[0-9]*?", music.df$release_date, value=TRUE, ignore.case=T, perl=T) #extracting all complete
+date_index <- which(music.df$release_date %in% temp.df) #finding indexed of the dates extracted in the last line
+processed.df <- music.df[date_index, ] #filtering the dataset using the indexes obtained
 dim(processed.df) #118188 rows
 
-#removing special charecters values from song names
-library(stringr)
-processed.df$name <- str_replace_all(processed.df$name, "[^[:alnum:]]", " ")
-processed.df$name <- str_replace_all(processed.df$name, "[[:punct:]]", " ")
+#Extracting month from release_date and adding the column as release_month
+library(lubridate)
+month <- month(as.POSIXlt(processed.df$release_date, format="%Y-%m-%d"))
+processed.df$release_month <- month
 
-#writing dataset to a file
-write.csv(processed.df,"C:\\Users\\nmala\\P2Records\\Data sets\\processed_music.csv", row.names = FALSE)
+#removing special characters values from song names
+library(stringr)
+processed.df$name <- gsub("[^[:alpha:]]","", processed.df$name)
+#contains(processed.df$name, "Î")
+#filter(processed.df, !grepl("Î", processed.df$name))
+
+
+#combining different terms in song name
+processed.df$name <- str_replace_all(processed.df$name, " ", "")
+
+#Removing special characters from artists
+processed.df$artists <- gsub("\\[|\\]", "", processed.df$artists)
+processed.df$artists <- gsub("\\'|\\'", "", processed.df$artists)
+processed.df$artists <- gsub("[^[:alnum:]\\,]","", processed.df$artists)
+
+#Removing id as it does not have any analytical value
+processed.df <- subset(processed.df, select = -c(id))
+
+#Final check fore writing to file
+View(processed.df)
+
+write.csv(processed.df,"C:\\Users\\nmala\\P2Records\\Datasets\\processed_music.csv", row.names = FALSE)
 
