c67231b79bd9f42b1e0e6e190147b7462facbd97
“[Malavika <[n.malavika@yahoo.com]>
2020-12-02 12:50:10 -0800

    Converted duration to seconds

--- Code/Preprocessing/pre_processing_databygenre.R
+++ Code/Preprocessing/pre_processing_databygenre.R
@@ -7,5 +7,14 @@ music_genre[is.na(music_genre)] <- 0
 library(stringr)
 music_genre$genres <- str_replace_all(music_genre$genres, " ", "_")
 
+
+#converting duration_ms to seconds and renaming to Avg song duration (sec)
+music_genre$"Avg song duration (sec)" <- music_genre$duration_ms/1000
+music_genre <- subset(music_genre, select = -c(duration_ms))
+
+
+#final check
+View(music_genre)
+
 #writing dataset to a file
 write.csv(music_genre,"C:\\Users\\nmala\\P2Records\\Datasets\\processed_data_by_genres.csv", row.names = FALSE)
