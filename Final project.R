88ee394933614c6b7967ff1570cb9c4e531ba536
Bahnny19 <34365873+Bahnny19@users.noreply.github.com>
2020-12-02 12:49:01 -0800

    Update Final project.R
    Pre-processing code for 
    1. artists dataset
    2. year dataset

--- Final project.R
+++ Final project.R
@@ -1,5 +1,37 @@
 library(ggplot2)
 library(caret)
 
-data.df <- read.csv("C:/Users/Bahnny/Desktop/MSIS Docs/Data analytics 510/spotify dataset/data.csv")
-View(data.df)
+songs.df <- read.csv("C:/Users/Bahnny/Desktop/MSIS Docs/Data analytics 510/spotify dataset/data.csv")
+genre.df <- read.csv("C:/Users/Bahnny/Desktop/MSIS Docs/Data analytics 510/spotify dataset/data_by_genres.csv")
+
+#remove records with non-latin words in artists column
+artists.df <- read.csv("C:/Users/Bahnny/Desktop/MSIS Docs/Data analytics 510/spotify dataset/data_w_genres.csv")
+artists.df <- na.omit(artists.df)
+View(artists.df)
+
+artists.df <- artists.df[1:27261,]
+
+artists.df$duration_ms <- artists.df$duration_ms/1000.0
+colnames(artists.df)[which(names(artists.df) == "duration_ms")] <- "Avg song duration (sec)"
+
+ggplot(data=artists.df)+ geom_point(aes(x=danceability, y=energy, color = loudness))
+ggplot(data=artists.df)+ geom_point(aes(x=danceability, y=energy, color = tempo))
+ggplot(data=artists.df)+ geom_point(aes(x=popularity, y=duration_ms, color = energy))
+ggplot(data=artists.df)+ geom_point(aes(x=speechiness, y=instrumentalness))
+
+
+year.df <- read.csv("C:/Users/Bahnny/Desktop/MSIS Docs/Data analytics 510/spotify dataset/data_by_year.csv")
+year.df <- na.omit(year.df)
+
+year.df$duration_ms <- year.df$duration_ms/1000.0
+colnames(year.df)[which(names(year.df) == "duration_ms")] <- "Avg song duration (sec)"
+
+View(year.df)
+
+
+ggplot(data=year.df)+ geom_point(aes(x=year, y=popularity))
+ggplot(data=year.df)+ geom_point(aes(x=year, y=energy))
+ggplot(data=year.df)+ geom_point(aes(x=year, y=danceability, color = tempo))
+
+write.csv(artists.df,"C:\\Users\\Bahnny\\Desktop\\MSIS Docs\\Data analytics 510\\P2Records\\artists.csv", row.names = FALSE)
+write.csv(year.df,"C:\\Users\\Bahnny\\Desktop\\MSIS Docs\\Data analytics 510\\P2Records\\year.csv", row.names = FALSE)
